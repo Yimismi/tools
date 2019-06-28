ebb3392e241b44b0533471ba94dbc50fba567d31
yimismi <1143937551@qq.com>
2019-06-28 17:46:44 +0800

    add shell

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
+#!/bin/sh
 rm -rf ./target
 go build bootstrap.go
 go build main.go
 mkdir target
 mkdir target/tools
+mkdir target/tools/bin
 mv bootstrap target
 cd target/tools
 cp  -r ../../config ./
 cp  -r ../../web ./
+cp ../../stop.sh ./bin
+cp ../../restart.sh ./bin
 cd -
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm -rf /var/local/tools
+mv ./target/tools /var/local/
