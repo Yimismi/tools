050c1035e06f4c873b956fa9d5a4538c42b276e5
yimismi <1143937551@qq.com>
2019-06-29 11:28:27 +0800

    improve shell

--- restart.sh
+++ restart.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 sh /var/local/tools/bin/stop.sh
-nohup /var/local/tools/bootstrap > /dev/null 2>&1 &
-
-PID=`ps aux | grep /var/local/tools/ | grep bootstrap | awk '{print $2}'`
-
-if [ -n "$PID" ]; then
-    echo "start server succeed. pid:$PID"
-else echo "start server failed"
-fi
+nohup /var/local/tools/bootstrap
